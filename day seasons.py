541ce2579f7c557ea8e3a15bbc93b5a1585f7f9e
Lokeshyb <112839064+Lokeshyb@users.noreply.github.com>
2022-10-25 10:47:19 +0530

    Update day seasons.py

--- day seasons.py
+++ day seasons.py
@@ -1,22 +1,28 @@
 month = input("Input the month (e.g. January, February etc.): ")
 day = int(input("Input the day: "))
 
-if month in ('January', 'February', 'March'):
+if month in ('January', 'February','December'):
 	season = 'winter'
-elif month in ('April', 'May', 'June'):
+elif month in ('march','April', 'May'):
 	season = 'summer'
-elif month in ('July', 'August', 'September'):
+elif month in ('june''July', 'August'):
 	season = 'spring'
+elif month in ('september','october','November'):
+        season='fall'
 else:
 	season = 'autumn'
 
-if (month == 'March') and (day > 19):
+
+if (month == 'March' and day >= 20):
 	season = 'summer'
-elif (month == 'June') and (day > 20):
+
+elif (month == 'June' and day >= 21):
 	season = 'summer'
-elif (month == 'September') and (day > 21):
+
+elif (month == 'September' and day>= 22):
 	season = 'autumn'
-elif (month == 'December') and (day > 20):
+
+elif (month == 'December' and day >= 21):
 	season = 'winter'
 
 print("Season is",season)
